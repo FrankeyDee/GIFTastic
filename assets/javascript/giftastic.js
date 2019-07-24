45286322f1923d4b4715386664b4290019047015
frankey darban <frankeydarban@gmail.com>
2019-07-24 00:14:24 -0700

    final functionality

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -12,60 +12,60 @@ $(document).ready(function() {
   function renderButton() {
     for (var i = 0; i < superheroes.length; i++) {
       // console.log(superheroes[i]);
-      $('.buttons').append($('<button class="button" value="value">'+superheroes[i]+'</button>'));
+      $('.buttons').append($('<button class="button">'+superheroes[i]+'</button>'));
     }
+    $('.button').on('click',function() {
+      $(".col-8").empty();
+      var thisHero = $(this).text();
+      thisHero = thisHero.replace(' ','+');
+      console.log(thisHero);
+      //define query URL
+      var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + thisHero + "&api_key=puTf1GlvsIxLOd1CQI1eAzM62hQaWLKU&limit=10");
+        // console.log(queryURL);
+      // connect to giphy API, create variables for results
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        var results = response.data;
+  
+        $('#results').empty();
+        for(var i = 0; i < 10; i++) {
+          var rating = results[i].rating;
+          var urlImg = results[i].images.original_still.url;
+          var urlGif = results[i].images.original.url;
+  
+          var beginDiv = $("<div class='gifBlock'>");
+          var img = '<img src= " ' + urlImg +
+              '" data-still=" ' + urlImg +
+              ' " data-animate=" ' + urlGif + '" data-state="still" class="thisGIF"><br>';
+          
+            $('.thisGIF').on('click', function(){
+              var state = $(this).attr('data-state');
+                if (state === 'still'){
+                  $(this).attr('src',$(this).attr('data-animate'));
+                  $(this).attr('data-state', 'animate');
+                } else {
+                  $(this).attr('src',$(this).attr('data-still'));
+                  $(this).attr('data-state', 'still')
+                }
+            })
+         
+          var rate = "<p>Rating: " + rating.toUpperCase();
+            beginDiv.append(rate,img);
+            $(".col-8").append(beginDiv);
+        }})
+      }    
+    )
   }
   renderButton();
 
   ///take value of 'input' when you click on 'submit' and add to the array
-  $('button').on('click', function() {
-    console.log('hi!');
+  $('#submit').on('click', function() {
+    superheroes.push($('#input').val());
+    $('.buttons').empty();
+    renderButton();
+    return
   })
 
-
-
-  //on click function to declare button value
-  $('.button').on('click',function() {
-    $(".col-8").empty();
-    var thisHero = $(this).text();
-    thisHero = thisHero.replace(' ','+');
-    // console.log(thisHero);
-    //define query URL
-    var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + thisHero + "&api_key=puTf1GlvsIxLOd1CQI1eAzM62hQaWLKU&limit=10");
-      // console.log(queryURL);
-    // connect to giphy API, create variables for results
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      var results = response.data;
-
-      $('#results').empty();
-      for(var i = 0; i < 10; i++) {
-        var rating = results[i].rating;
-				var urlImg = results[i].images.original_still.url;
-        var urlGif = results[i].images.original.url;
-
-        var beginDiv = $("<div class='gifBlock'>");
-				var img = '<img src= " ' + urlImg +
-            '" data-still=" ' + urlImg +
-            ' " data-animate=" ' + urlGif + '" data-state="still" class="thisGIF"><br>';
-        
-          $('.thisGIF').on('click', function(){
-            var state = $(this).attr('data-state');
-              if (state === 'still'){
-                $(this).attr('src',$(this).attr('data-animate'));
-                $(this).attr('data-state', 'animate');
-              } else {
-                $(this).attr('src',$(this).attr('data-still'));
-                $(this).attr('data-state', 'still')
-              }
-          })
-       
-        var rate = "<p>Rating: " + rating.toUpperCase();
-				  beginDiv.append(rate,img);
-				  $(".col-8").append(beginDiv);
-      }})
-    }    
-  )
 });
